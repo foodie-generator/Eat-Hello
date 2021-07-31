b416a8a39ea156e57fc025e45b65c634db9b7216
Hazel <hazel.na3@gmail.com>
2021-07-30 20:59:27 -0400

    update 1.4

--- client/components/foodGenerator.js
+++ client/components/foodGenerator.js
@@ -1,10 +1,12 @@
-import React, { Component, useEffect } from 'react';
+import React, { Component, useState } from 'react';
 
 const FoodGenerator = () => {
+  const [menu, setMenu] = useState('Hmm... any idea? 🤔');
+
   return (
     <div className='food_generator'>
-      <div>what should we eat?</div>
-      <input type='text' placeholder='what is your food today?' />
+      <div id='generator_title'>what should we eat?</div>
+      <div id='generator_menu'> {menu} </div>
     </div>
   );
 };
