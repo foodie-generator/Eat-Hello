ec0cf34e4216c4bb765d0ec7510f36a4e8190359
emeric-gh <emericbdavid@gmail.com>
2021-07-31 12:11:49 -0400

    commit to pull

--- client/popUps/popup.js
+++ client/popUps/popup.js
@@ -3,8 +3,10 @@ import React, { Component, useEffect } from 'react';
 
 const Popup = (props) => {
   return (
-    <div>
-      <h1 > test </h1>
+    <div className="popUp">
+      <div className="popUpBody">
+        <h1 > {props.display} </h1>
+      </div>
     </div>
   );
 }
