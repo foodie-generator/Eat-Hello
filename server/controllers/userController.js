eac708f2deb75cc3ff1d7147e9ad483744ad5a07
addakridler <addakridler@gmail.com>
2021-07-31 18:27:58 -0400

    eslint installation

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,88 +1,67 @@
-const User = require('../models/userModel');
-const userController = {};
 const bcrypt = require('bcryptjs');
-​
-/**
-* getAllUsers - retrieve all users from the database and stores it into res.locals
-* before moving on to next middleware.
-*/
+const User = require('../models/userModel');
 
-// userController.getAllUsers = (req, res, next) => {
-//   User.find({}, (err, users) => {
-//     // if a database error occurs, call next with the error message passed in for the express global error handler to catch
-//     if (err) return next('Error in userController.getAllUsers: ' + JSON.stringify(err));
-//     // store retrieved users into res.locals and move on to next middleware
-//     res.locals.users = users;
-//     return next();
-//   });
-// };
-​
+const userController = {};
 /**
 * createUser - create and save a new User into the database.
 */
-userController.createUser = async(req, res, next) => {
-    try {
-      const { firstName, lastName, location, preferCuisine, username, password } = req.params;
-      console.log(req.params)
-      const newUser = {
-        firstName: firstName,
-        lastName: lastName,
-        location: location,
-        preferCuisine: preferCuisine,
-        username: username,
-        password: password,  
-      };
-      const result = await User.create(newUser);
-      // const userID = result._doc_doc._id.id
-      // res.locals.said = userID;
-      console.log(result);
-      return next();
-    }
-    catch(err) {
-      return next(err);
-    }
-  };
-​
-
+userController.createUser = async (req, res, next) => {
+  try {
+    const {
+      firstName, lastName, location, preferCuisine, username, password,
+    } = req.params;
+    console.log(req.params);
+    const newUser = {
+      firstName,
+      lastName,
+      location,
+      preferCuisine,
+      username,
+      password,
+    };
+    const result = await User.create(newUser);
+    // const userID = result._doc_doc._id.id
+    // res.locals.said = userID;
+    console.log(result);
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+};
 /** ssid? */
-​
-​
+
 /**
 * verifyUser - Obtain username and password from the request body, locate
 * the appropriate user in the database, and then authenticate the submitted password
 * against the password stored in the database.
 */
-userController.verifyUser = async(req, res, next) => {
+userController.verifyUser = async (req, res, next) => {
   try {
     const { username, password } = req.params;
     const userQuery = {
-      username: username,
-    }
-    //look in our database and find an entry with user's username
-    const result = await user.findOne(userQuery);
+      username,
+    };
+    // look in our database and find an entry with user's username
+    const result = await User.findOne(userQuery);
     console.log(result);
-    //if we find it, compare user's password with database's password 
-    const passwordCompare = await bcrypt.compare(password, result._doc.password);
+    // if we find it, compare user's password with database's password
+    const passwordCompare = await bcrypt.compare(password, result.password);
     // get mongo id from result body and set on res locals for cookies
-    // to compare the user's input(hased) to verify - if true return next 
+    // to compare the user's input(hased) to verify - if true return next
     if (passwordCompare) return next();
-    else return next({
-      err: console.log('passwords do not match')
-    }); 
+    return next({
+      err: console.log('passwords do not match'),
+    });
+  } catch (err) {
+    return (console.log('userController.verifyUser error'));
   }
-  catch(err) {
-    console.log('userController.verifyUser error')
-  }
-​
 };
-​
 /**
- * to access the user's data after the user logs in and in order to acess the user's location, 
+ * to access the user's data after the user logs in and in order to acess the user's location,
  * prefer cousine and food/ answer the user got from the past
 */
 userController.profileData = (req, res, next) => {
   // write code here
-​
 };
-​
+
 module.exports = userController;
--- server/server.js
+++ server/server.js
@@ -2,34 +2,28 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = 3000;
-const mongoose = require('mongoose');
-const userController = require('./controllers/userController');
-const cookieParser = require('cookie-parser');
+//const mongoose = require('mongoose');
+//const userController = require('./controllers/userController');
+//const cookieParser = require('cookie-parser');
 const cuisineRouter = require('./routes/cuisine');
 
 // activate the cookieParser 
-app.use(cookieParser());
-​
-​
-// activate the parser
-​app.use(express.json());
-
+//app.use(cookieParser());
+app.use(express.json());
 
 // a start point for the routes
 app.use('/api', cuisineRouter);
 
-​
+
 if (process.env.NODE_ENV === 'production') {
   app.use('/build', express.static(path.join(__dirname, '../build')));
   app.get('/', (req, res) => {
     return res.status(200).sendFile(path.join(__dirname, '../index.html'));
   });
 }
-​
-​
 // error handlers
 app.use('*',(req, res) => res.status(404).sendFile(path.join(__dirname, '/client/404.html')));
-​
+
 // global error handler 
 app.use(function (err, req, res, next) {
   const defaultErr = {
@@ -41,12 +35,12 @@ app.use(function (err, req, res, next) {
   // console.log(errorObj.log);
   return res.status(errorObj.status).json(errorObj.message);
 });
-​
-​
-​
+
+
+
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 });
-​
+
 module.exports = app; //-> http://localhost:3000/
 // localhost:8080
