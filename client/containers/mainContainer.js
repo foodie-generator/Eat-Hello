d97f32e3c3e182bffc8d4d05c976fe5dfd3553cf
emeric-gh <emericbdavid@gmail.com>
2021-08-02 14:39:13 -0400

    content function for popup header

--- client/containers/mainContainer.js
+++ client/containers/mainContainer.js
@@ -5,12 +5,13 @@ import FoodPicker from '../components/foodpicker';
 import Footer from '../components/footer';
 
 const MainContainer = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
   const [menu, setMenu] = useState('');
   const [zipcode, setZipcode] = useState(0);
 
   return (
     <div className='main_container'>
-      <Header />
+      <Header loggedIn={loggedIn}/>
       <div className='main'>
         <FoodGenerator menu={menu} />
         <FoodPicker
--- client/popUps/popup.js
+++ client/popUps/popup.js
@@ -1,11 +1,52 @@
-import React, { Component, useEffect } from 'react';
-
+import { PromiseProvider } from 'mongoose';
+import React, { Component, Fragment, useEffect } from 'react';
+import goblinShark from '../Images/goboshark.png';
 
 const Popup = (props) => {
+
+  const logInContent = ()=>{
+    //is user logged in 
+    if(props.loggedIn){
+      return(
+        <Fragment>
+          <h2>
+            Welcome Back
+          </h2>
+        </Fragment>
+      );
+    }//else not logged in
+
+    return(
+      <div className='logInButtonContainer' >
+        <button>
+          Sign up
+        </button>
+        <button>
+          Log In
+        </button>
+      </div>
+    );
+  };
+
+  const catContent = ()=>{
+    return(
+      <Fragment>
+        <img src={goblinShark} alt="goblin-shark" />
+      </Fragment>
+    );
+  };
+
+  const content = ()=>{
+    //these could be done without strings to controll content state
+    if(props.display === 'signIn') return logInContent();
+    if(props.display === 'cat') return catContent();
+    //else
+    return <h1 > {props.display} </h1>;
+  };
   return (
     <div className="popUp">
       <div className="popUpBody">
-        <h1 > {props.display} </h1>
+        {content()}
       </div>
     </div>
   );
