a4a1fa7489ae843c153b171b040cd00f8b50e673
Hazel <hazel.na3@gmail.com>
2021-08-02 18:33:08 -0400

    push

--- client/popUps/historyPopup.js
+++ client/popUps/historyPopup.js
@@ -6,18 +6,19 @@ const HistoryPopup = ({ menu, username }) => {
   // fetch request to database to get user history using username
   const foodHistory = async (username) => {
     try {
-      await axios.get('/api/foodHistory', username); 
+      await axios.get('/api/foodHistory', username);
     } catch (err) {
       console.log('err: ', err);
-    };
-  // render the user history array
-  // on menu state change update user food history in the database
+    }
+    // render the user history array
+    // on menu state change update user food history in the database
 
-  return (
-    <div className='history_popUp'>
-      <div className='history_popUpBody'>{foodHistory}</div>
-    </div>
-  );
+    return (
+      <div className='history_popUp'>
+        <div className='history_popUpBody'>{foodHistory}</div>
+      </div>
+    );
+  };
 };
 
 export default HistoryPopup;
