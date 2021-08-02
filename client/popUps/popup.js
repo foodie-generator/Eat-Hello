64fc941f4771cbd0ad698732e1609449e4224b2c
Hazel <hazel.na3@gmail.com>
2021-08-02 17:04:13 -0400

    popup3

--- client/popUps/popup.js
+++ client/popUps/popup.js
@@ -2,72 +2,63 @@ import { PromiseProvider } from 'mongoose';
 import React, { Component, Fragment, useEffect } from 'react';
 import goblinShark from '../Images/goboshark.png';
 
-const Popup = (props) => {
-
-  const logInContent = ()=>{
-    //is user logged in 
-    if(props.loggedIn){
-      return(
+const Popup = ({ display, loggedIn, displayLoginForm }) => {
+  const logInContent = () => {
+    //is user logged in
+    if (loggedIn) {
+      return (
         <Fragment>
-          <h2>
-            Welcome Back
-          </h2>
+          <h2>Welcome Back</h2>
         </Fragment>
       );
-    }//else not logged in
+    } //else not logged in
 
-    if(props.displayLoginForm){
-      return(
+    if (displayLoginForm) {
+      return (
         <Fragment>
-          <div className="card">
+          <div className='card'>
             <h2>Credentials</h2>
-            <label className="input">
-              <input className="input__field" type="text" placeholder=" " />
-              <span className="input__label">User Name</span>
+            <label className='input'>
+              <input className='input__field' type='text' placeholder=' ' />
+              <span className='input__label'>User Name</span>
             </label>
-            <div className="button-group">
+            <div className='button-group'>
               <button>Send</button>
-              <button type="reset">Reset</button>
+              <button type='reset'>Reset</button>
             </div>
           </div>
         </Fragment>
-      )
+      );
     }
 
-    return(
-      <div className='logInButtonContainer' >
-        <button className='submit signUpButton' >
-          Sign up
-        </button>
-        <button className='submit logInButton' >
-          Log In
-        </button>
+    return (
+      <div className='logInButtonContainer'>
+        <button className='submit signUpButton'>Sign up</button>
+        <button className='submit logInButton'>Log In</button>
       </div>
     );
   };
 
-  const catContent = ()=>{
-    return(
+  const catContent = () => {
+    return (
       <Fragment>
-        <img src={goblinShark} alt="goblin-shark" />
+        <img src={goblinShark} alt='goblin-shark' />
       </Fragment>
     );
   };
 
-  const content = ()=>{
+  const content = () => {
     //these could be done without strings to controll content state
-    if(props.display === 'signIn') return logInContent();
-    if(props.display === 'cat') return catContent();
+    if (display === 'signIn') return logInContent();
+    if (display === 'cat') return catContent();
     //else
-    return <h1 > {props.display} </h1>;
+    return <h1> {display} </h1>;
   };
   return (
-    <div className="popUp">
-      <div className="popUpBody">
-        {content()}
-      </div>
+    <div className='popUp'>
+      <div className='popUpBody'>{content()}</div>
     </div>
   );
-}
+};
 
 export default Popup;
